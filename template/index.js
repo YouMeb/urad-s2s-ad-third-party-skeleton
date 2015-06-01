40d4bce450f28cfb0447db603f2dc3958e81e005
Po-Ying Chen <poying.me@gmail.com>
2015-06-01 13:56:50 +0800

    remove getTransaction method

--- template/index.js
+++ template/index.js
@@ -7,16 +7,5 @@ module.exports = ThirdParty({
 
   ipWhitelist: [
     // 請填入 ip address
-  ],
-
-  getTransaction(postbackContext) {
-    return {
-      click_id: postbackContext.{{trans_clickid}},
-      imei: postbackContext.{{trans_imei}},
-      idfa: postbackContext.{{trans_idfa}},
-      android_id: postbackContext.{{trans_android_id}},
-      advertising_id: postbackContext.{{trans_advertising_id}},
-      data: postbackContext.query
-    };
-  }
+  ]
 });
